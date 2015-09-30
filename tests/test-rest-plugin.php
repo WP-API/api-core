cdf70a971408daa583c3d918f5af39e6114187b6
Rachel Baker <rachel@rachelbaker.me>
2015-09-30 17:40:49 -0500

    Remove controller dependent tests left in test-rest-plugin.php

--- tests/test-rest-plugin.php
+++ tests/test-rest-plugin.php
@@ -235,41 +235,6 @@ class WP_Test_REST_Plugin extends WP_UnitTestCase {
 		$this->assertNull( $response );
 	}
 
-	public function test_add_extra_api_taxonomy_arguments() {
-		$this->markTestSkipped();
-
-		// bootstrap the taxonomy variables
-		_add_extra_api_taxonomy_arguments();
-
-		$taxonomy = get_taxonomy( 'category' );
-		$this->assertTrue( $taxonomy->show_in_rest );
-		$this->assertEquals( 'category', $taxonomy->rest_base );
-		$this->assertEquals( 'WP_REST_Terms_Controller', $taxonomy->rest_controller_class );
-
-		$taxonomy = get_taxonomy( 'post_tag' );
-		$this->assertTrue( $taxonomy->show_in_rest );
-		$this->assertEquals( 'tag', $taxonomy->rest_base );
-		$this->assertEquals( 'WP_REST_Terms_Controller', $taxonomy->rest_controller_class );
-	}
-
-	public function test_add_extra_api_post_type_arguments() {
-
-		$post_type = get_post_type_object( 'post' );
-		$this->assertTrue( $post_type->show_in_rest );
-		$this->assertEquals( 'posts', $post_type->rest_base );
-		$this->assertEquals( 'WP_REST_Posts_Controller', $post_type->rest_controller_class );
-
-		$post_type = get_post_type_object( 'page' );
-		$this->assertTrue( $post_type->show_in_rest );
-		$this->assertEquals( 'pages', $post_type->rest_base );
-		$this->assertEquals( 'WP_REST_Posts_Controller', $post_type->rest_controller_class );
-
-		$post_type = get_post_type_object( 'attachment' );
-		$this->assertTrue( $post_type->show_in_rest );
-		$this->assertEquals( 'media', $post_type->rest_base );
-		$this->assertEquals( 'WP_REST_Attachments_Controller', $post_type->rest_controller_class );
-	}
-
 	/**
 	 * The get_rest_url function should return a URL consistently terminated with a "/",
 	 * whether the blog is configured with pretty permalink support or not.
